fa19fc491792e037dc38c0c9bdf9709a2cf2190e
Srishti Gupta <47494059+srishti1723@users.noreply.github.com>
2020-01-28 23:55:42 +0530

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,18 @@
+<script>
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  </script>
