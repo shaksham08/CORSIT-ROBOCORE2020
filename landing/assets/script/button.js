d5b2c63299a88133e0547c648f6b5d2b63a961d7
shaksham08 <shakshamsinha@outlook.com>
2020-02-03 23:57:23 +0530

    Downling this without permission is bad

--- landing/assets/script/button.js
+++
@@ -1,8 +0,0 @@
-var btn = document.querySelector(".btn");
-
-btn.addEventListener("mouseenter", function() {
-  this.textContent = "Completed!";
-})
-btn.addEventListener("mouseout", function() {
-  this.textContent = "New Request";
-})
